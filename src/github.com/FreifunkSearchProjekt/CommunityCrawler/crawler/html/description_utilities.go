838d2794d4564b38164193501c27a77a23772a95
MTRNord <mtrnord1@gmail.com>
2018-01-08 15:17:08 +0100

    Optimize getting the description by following the HTML5 guidelines
    Signed-off-by: MTRNord <mtrnord1@gmail.com>

--- src/github.com/FreifunkSearchProjekt/CommunityCrawler/crawler/html/description_utilities.go
+++ src/github.com/FreifunkSearchProjekt/CommunityCrawler/crawler/html/description_utilities.go
@@ -7,10 +7,50 @@ import (
 )
 
 func GetDescription(htm *goquery.Document) string {
-	p := htm.Find("p:first-of-type").Map(func(_ int, s *goquery.Selection) string {
+	// Check if we got a article tag
+	article := htm.Find("article").Map(func(_ int, s *goquery.Selection) string {
 		val, _ := s.Html()
 		return val
 	})
+
+	// If we got a article only search in there for p tags else just get all of them and hope for the best.
+	var p []string
+	if len(article) > 0 {
+		p = htm.Find("article").Find("p").Map(func(_ int, s *goquery.Selection) string {
+			val, _ := s.Html()
+			return val
+		})
+	} else {
+		// Check if we got a footer and if we can exclude it
+		footer := htm.Find("footer").Map(func(_ int, s *goquery.Selection) string {
+			val, _ := s.Html()
+			return val
+		})
+
+		if len(footer) > 0 {
+			p = htm.Not("footer").Find("p").Map(func(_ int, s *goquery.Selection) string {
+				val, _ := s.Html()
+				return val
+			})
+		} else {
+			// Check if we got a header and if we can exclude it
+			header := htm.Find("header").Map(func(_ int, s *goquery.Selection) string {
+				val, _ := s.Html()
+				return val
+			})
+			if len(header) > 0 {
+				p = htm.Not("header").Find("p").Map(func(_ int, s *goquery.Selection) string {
+					val, _ := s.Html()
+					return val
+				})
+			} else {
+				p = htm.Find("p").Map(func(_ int, s *goquery.Selection) string {
+					val, _ := s.Html()
+					return val
+				})
+			}
+		}
+	}
 
 	if len(p) > 0 {
 		joined := strings.Join(p[:], " ")
